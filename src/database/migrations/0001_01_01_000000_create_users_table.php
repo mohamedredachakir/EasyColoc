1abd06dbd681b2f9b47af6a2f1b24bfbc0ee496f
reddcs <reddcs@gmail.com>
2026-02-24 13:05:49 +0000

    feat: update users table with role, reputation, and ban status

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Enums\RoleEnum;
 return new class extends Migration
 {
     /**
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', [RoleEnum::ADMIN, RoleEnum::OWNER, RoleEnum::MEMBER, RoleEnum::GUEST])->defaultTo(RoleEnum::GUEST);
+            $table->integer('reputation')->default(0);
+            $table->boolean('is_banned')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
