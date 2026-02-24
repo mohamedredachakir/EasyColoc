6a0f79e8497a5cb7bb968c3330fd2e9153d58cf7
reddcs <reddcs@gmail.com>
2026-02-24 13:05:56 +0000

    feat: add migrations for colocations and colocation_users

--- src/database/migrations/2026_02_24_112731_create_colocations_table.php
+++ src/database/migrations/2026_02_24_112731_create_colocations_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\ColocationStatus;
 
 return new class extends Migration
 {
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('colocations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
+            $table->enum('status', [ColocationStatus::ACTIVE, ColocationStatus::CANCELLED])->default(ColocationStatus::ACTIVE);
             $table->timestamps();
         });
     }
--- src/database/migrations/2026_02_24_112753_create_colocation_users_table.php
+++ src/database/migrations/2026_02_24_112753_create_colocation_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('colocation_users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('colocation_id')->constrained('colocations')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamp('joined_at')->useCurrent();
+            $table->timestamp('left_at')->nullable();
             $table->timestamps();
         });
     }
