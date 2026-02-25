87d677d60623bb8d6c45b8b6b8de604ed84c97e3
reddcs <reddcs@gmail.com>
2026-02-25 10:09:58 +0000

    feat: Implement full CRUD operations for colocations, including authorization and data loading, and register the role middleware.

--- src/app/Http/Controllers/ColocationController.php
+++ src/app/Http/Controllers/ColocationController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Models\Colocation;
+use App\Models\Expense;
+use App\Models\Category;
+use App\Models\Invitation;
+use Illuminate\Http\Request;
+use App\Enums\ColocationStatus;
 class ColocationController extends Controller
 {
     /**
@@ -11,7 +17,9 @@ class ColocationController extends Controller
      */
     public function index()
     {
-        //
+        $user = auth()->user();
+        $colocations = $user->colocations()->with('users','expenses','categories')->get();
+        return view('colocations.index', compact('colocations'));
     }
 
     /**
@@ -19,7 +27,7 @@ class ColocationController extends Controller
      */
     public function create()
     {
-        //
+        return view('colocations.create');
     }
 
     /**
@@ -27,38 +35,73 @@ class ColocationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validateWithBag('colocation', [
+            'name' => ['required|string|max:255'],
+        ]);
+
+        $colocation = Colocation::create([
+            'name' => $request->name,
+            'user_id' => auth()->id(),
+            'status' =>  ColocationStatus::ACTIVE,
+        ]);
+
+        if(!$colocation){return redirect()->back()->with('error', 'Colocation not created.');}
+
+        return redirect()->route('colocations.index')->with('success', 'Colocation created successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Colocation $colocation)
     {
-        //
+        $colocation->load('users','expenses.payer','categories','invitations.receiver');
+        $members = $colocation->users->map(function($user) use ($colocation) {
+        $totalPaid = $colocation->expenses->where('payer_id', $user->id)->sum('amount');
+        $share = $colocation->expenses->sum('amount') / $colocation->users->count();
+        $balance = $totalPaid - $share;
+        $user->balance = $balance;
+        return $user;
+    });
+
+        return view('colocations.show', compact('colocation'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Colocation $colocation)
     {
-        //
+        $this->authorize('update', $colocation);
+        return view('colocations.edit', compact('colocation'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Colocation $colocation)
     {
-        //
+        $this->authorize('update', $colocation);
+        $request->validate([
+        'name' => 'required|string|max:255',
+        ]);
+
+        $colocation->update([
+            'name' => $request->name,
+        ]);
+
+        if(!$colocation){return redirect()->back()->with('error', 'Colocation not updated.');}
+
+        return redirect()->route('colocations.index')->with('success', 'Colocation updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Colocation $colocation)
     {
-        //
+        $this->authorize('delete', $colocation);
+        $colocation->delete();
+        return redirect()->route('colocations.index')->with('success', 'Colocation deleted successfully.');
     }
 }
